b933164319153b2e964f9d6a42b435850f24d68b
Justin Lee <justinl7921@gmail.com>
2026-07-17 19:49:31 -0500

    added react, motion, finished footer (for the most part), and added decrypting text to homepage h1

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,47 +2,53 @@
 import { defineConfig, fontProviders } from 'astro/config';
 import tailwind from '@tailwindcss/vite';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://JustinL284.github.io',
-    base: '/personal-portfolio',
-    fonts: [{
-        provider: fontProviders.local(),
-        name: "ClashDisplay-Variable",
-        cssVariable: "--font-clash-display",
-        options: {
-        variants: [{
-            src: ['./src/fonts/ClashDisplay-Variable.ttf'],
-            weight: "100 900",
-            style: 'normal'
-        }]
-        }
-    },
-    {
-        provider: fontProviders.local(),
-        name: "Supreme-Variable",
-        cssVariable: "--font-supreme",
-        options: {
-        variants: [{
-            src: ['./src/fonts/Supreme-Variable.ttf'],
-            weight: "100 900",
-            style: 'normal'
-        }]
-        }
-    },
-    {
-        provider: fontProviders.local(),
-        name: "Supreme-VariableItalic",
-        cssVariable: "--font-supreme-Italic",
-        options: {
-        variants: [{
-            src: ['./src/fonts/Supreme-VariableItalic.ttf'],
-            weight: "100 900",
-            style: 'italic'
-        }]
-        }
-    }],
-    vite: {
-        plugins: [tailwind()],
-    }
+  site: 'https://JustinL284.github.io',
+  base: '/personal-portfolio',
+
+  fonts: [{
+      provider: fontProviders.local(),
+      name: "ClashDisplay-Variable",
+      cssVariable: "--font-clash-display",
+      options: {
+      variants: [{
+          src: ['./src/fonts/ClashDisplay-Variable.ttf'],
+          weight: "100 900",
+          style: 'normal'
+      }]
+      }
+  },
+  {
+      provider: fontProviders.local(),
+      name: "Supreme-Variable",
+      cssVariable: "--font-supreme",
+      options: {
+      variants: [{
+          src: ['./src/fonts/Supreme-Variable.ttf'],
+          weight: "100 900",
+          style: 'normal'
+      }]
+      }
+  },
+  {
+      provider: fontProviders.local(),
+      name: "Supreme-VariableItalic",
+      cssVariable: "--font-supreme-Italic",
+      options: {
+      variants: [{
+          src: ['./src/fonts/Supreme-VariableItalic.ttf'],
+          weight: "100 900",
+          style: 'italic'
+      }]
+      }
+  }],
+
+  vite: {
+      plugins: [tailwind()],
+  },
+
+  integrations: [react()]
 });
